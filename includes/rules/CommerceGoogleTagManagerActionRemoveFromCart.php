88e8dc6839a6cf92a816506a719c4b37f5f5114e
thePanz <thepanz@gmail.com>
2016-03-17 13:39:49 +0100

    Added "Remove from Cart" action

---
+++ includes/rules/CommerceGoogleTagManagerActionRemoveFromCart.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * This file contains the "Send Remove From Cart event to GTM" action definition.
+ */
+class CommerceGoogleTagManagerActionRemoveFromCart extends CommerceGoogleTagManagerBaseAction {
+
+  /**
+   * Defines the action.
+   *
+   * @return array
+   */
+  public static function getInfo() {
+    return self::getDefaultsInfo() + array(
+      'label' => t('Send "Remove from Cart" event to google analytics via GTM'),
+      'parameter' => array(
+        'item' => array(
+          'type' => 'commerce_line_item',
+          'label' => t('Line Item in the Order'),
+        ),
+        'count' => array(
+          'type' => 'text',
+          'label' => t('The count of items added to the Cart'),
+          'optional' => TRUE,
+        ),
+      ),
+    );
+  }
+
+  /**
+   * @return string
+   */
+  public function getCommerceEventName() {
+    return 'removeFromCart';
+  }
+
+  /**
+   * Executes the action.
+   *
+   * @param array $item The Line Item of the order
+   * @param string $count Number of items added to the order
+   */
+  public function execute($item, $count) {
+    $item = CommerceGoogleTagManagerHelper::getWrappedLineItem($item);
+
+    $productData = CommerceGoogleTagManagerHelper::getLineItemData($item, $this->getCommerceEventName());
+    $order = $item->order;
+    $currencyCode = $order->commerce_order_total->currency_code->value();
+
+    // Override the product quantity, if set:
+    if ($count) {
+      $productData['quantity'] = $count;
+    }
+
+    $data = array(
+      'currencyCode' => $currencyCode,
+      'remove' => array(
+        'products' => array($productData),
+      ),
+    );
+
+    $this->pushCommerceData($data);
+  }
+}
