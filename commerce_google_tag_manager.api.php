140e4aac89a4237189072ead634569308cffa65c
nielsdefeyter <nielsdefeyter@599438.no-reply.drupal.org>
2016-06-29 11:16:05 +0200

    Issue #2728361 by ndf, thePanz: Add a Hook to alter aggregated data, just before pushing to the Datalayer

--- commerce_google_tag_manager.api.php
+++ commerce_google_tag_manager.api.php
@@ -36,7 +36,7 @@ function hook_commerce_google_tag_manager_order_data_alter(&$order_data, $contex
 }
 
 /**
- * Allow other modules to alter the data before pushing to DataLayer.
+ * Allow other modules to alter data for a single event before aggregation.
  * The $data array structure changes depending on the event that is executed.
  *
  * The $context variable is an associative array with the following keys:
@@ -47,5 +47,43 @@ function hook_commerce_google_tag_manager_commmerce_data_alter(&$data, $context)
   if ($context['event'] == 'checkout') {
     // Change the 'option' data
     $data['ecommerce']['checkout']['actionField']['option'] = 'Overwritten Option';
+  }
+}
+
+/**
+ * Allow other modules to alter aggregated event-data just before pushing to
+ * the DataLayer.
+ *
+ * This hook is called within page_build().
+ */
+function hook_commerce_google_tag_manager_commerce_data_aggregated_alter(&$data) {
+  // Reorder the events by priority: most important events on top.
+  $prio_events = array(
+    '"event":"purchase"',
+    '"event":"addToCart"',
+    '"event":"removeFromCart"',
+    '"event":"checkout"',
+  );
+
+  // Build a string representation of the data, to be able to match our events.
+  $imploded_data = implode('', $data);
+
+  // This double loop replaces multiple event-scripts with the script from the
+  // most important event.
+  foreach ($prio_events as $event) {
+    // Find needle in scripts-array.
+    if (strpos($imploded_data, $event) !== FALSE) {
+      // Find specific script and select it.
+      foreach ($data as $script) {
+        if (strpos($script, $event) !== FALSE) {
+          // Replace the array with multiple event-scripts by the prio-event.
+          $data = array($script);
+          // Exit inner loop.
+          continue;
+        }
+      }
+      // Exit outer loop.
+      continue;
+    }
   }
 }
