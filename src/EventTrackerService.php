1cc2ac68eb1f20dc58f76ffc3509bc4a24766ae4
Kevin Wenger <wenger.kev@gmail.com>
2018-11-05 10:40:20 +0100

    fix crash purchase Entity - When 'Commerce Shipping' not enabled - close #2

--- src/EventTrackerService.php
+++ src/EventTrackerService.php
@@ -362,7 +362,7 @@ class EventTrackerService {
    * @return float
    */
   private function calculateShipping(OrderInterface $order) {
-    if ($order->get('shipments') && !$order->get('shipments')->isEmpty()) {
+    if ($order->hasField('shipments') && !$order->get('shipments')->isEmpty()) {
       $total = 0;
       foreach ($order->get('shipments')->referencedEntities() as $shipment) {
         /** @var \Drupal\commerce_shipping\Entity\ShipmentInterface $shipment */
