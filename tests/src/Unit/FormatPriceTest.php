534991a673c598a43c6541c64a29f2b1fc830712
Kevin Wenger <wenger.kev@gmail.com>
2019-08-15 15:05:12 +0200

    Issue #3056903 by eiriksm, wengerk: Make formatPrice method static

--- tests/src/Unit/FormatPriceTest.php
+++ tests/src/Unit/FormatPriceTest.php
@@ -4,12 +4,6 @@ namespace Drupal\Tests\commerce_google_tag_manager\Unit;
 
 use Drupal\Tests\UnitTestCase;
 use Drupal\commerce_google_tag_manager\EventTrackerService;
-use Drupal\commerce_google_tag_manager\EventStorageService;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Drupal\Tests\commerce_google_tag_manager\Traits\InvokeMethodTrait;
-use Drupal\commerce_store\CurrentStoreInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\commerce_order\PriceCalculatorInterface;
 
 /**
  * Tests the formatPrice of EventTrackerService class.
@@ -21,33 +15,6 @@ use Drupal\commerce_order\PriceCalculatorInterface;
  * @group commerce_google_tag_manager_unit
  */
 class FormatPriceTest extends UnitTestCase {
-  use InvokeMethodTrait;
-
-  /**
-   * The Commerce GTM event tracker.
-   *
-   * @var \Drupal\commerce_google_tag_manager\EventTrackerService
-   */
-  private $eventTracker;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-
-    $event_storage = $this->prophesize(EventStorageService::class);
-    $event_dispatcher = $this->prophesize(EventDispatcherInterface::class);
-
-    $current_store = $this->prophesize(CurrentStoreInterface::class);
-    $current_user = $this->prophesize(AccountInterface::class);
-    $price_calculator = $this->prophesize(PriceCalculatorInterface::class);
-
-    $store = $this->prophesize(StoreInterface::class);
-    $current_store->getStore()->willReturn($store->reveal());
-
-    $this->eventTracker = new EventTrackerService($event_storage->reveal(), $event_dispatcher->reveal(), $current_store->reveal(), $current_user->reveal(), $price_calculator->reveal());
-  }
 
   /**
    * @covers ::formatPrice
@@ -55,7 +22,7 @@ class FormatPriceTest extends UnitTestCase {
    * @dataProvider pricesProvider
    */
   public function testFormatPrice($price, $expected) {
-    $result = $this->invokeMethod($this->eventTracker, 'formatPrice', [$price]);
+    $result = EventTrackerService::formatPrice($price);
     $this->assertEquals($expected, $result);
   }
 
