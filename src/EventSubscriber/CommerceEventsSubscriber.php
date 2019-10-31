e52bb78a959bc102208de3061a63ccba43333969
wengerk <wengerk@2794871.no-reply.drupal.org>
2019-10-31 14:52:27 +0100

    Issue #3074305 by zeuty, wengerk: productDetailViews not fired because hook_entity_view is not invoked on every product page load

--- src/EventSubscriber/CommerceEventsSubscriber.php
+++ src/EventSubscriber/CommerceEventsSubscriber.php
@@ -6,8 +6,11 @@ use Drupal\commerce_cart\Event\CartEntityAddEvent;
 use Drupal\commerce_cart\Event\CartEvents;
 use Drupal\commerce_cart\Event\CartOrderItemRemoveEvent;
 use Drupal\commerce_google_tag_manager\EventTrackerService;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\state_machine\Event\WorkflowTransitionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * Event handler for commerce related events.
@@ -22,13 +25,23 @@ class CommerceEventsSubscriber implements EventSubscriberInterface {
   private $eventTracker;
 
   /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
    * Constructs the CommerceEventsSubscriber object.
    *
    * @param \Drupal\commerce_google_tag_manager\EventTrackerService $event_tracker
    *   The Commerce GTM event tracker.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match, for context.
    */
-  public function __construct(EventTrackerService $event_tracker) {
+  public function __construct(EventTrackerService $event_tracker, RouteMatchInterface $route_match) {
     $this->eventTracker = $event_tracker;
+    $this->routeMatch = $route_match;
   }
 
   /**
@@ -39,6 +52,9 @@ class CommerceEventsSubscriber implements EventSubscriberInterface {
       CartEvents::CART_ENTITY_ADD => 'trackCartAdd',
       CartEvents::CART_ORDER_ITEM_REMOVE => 'trackCartRemove',
       'commerce_order.place.post_transition' => 'trackPurchase',
+      // trackProductView should run before Dynamic Page Cache, which has priority 27
+      // see Drupal\dynamic_page_cache\EventSubscriber\DynamicPageCacheSubscriber
+      KernelEvents::REQUEST => ['trackProductView', 28]
     ];
   }
 
@@ -50,6 +66,23 @@ class CommerceEventsSubscriber implements EventSubscriberInterface {
    */
   public function trackCartAdd(CartEntityAddEvent $event) {
     $this->eventTracker->addToCart($event->getOrderItem(), (int) $event->getQuantity());
+  }
+
+  /**
+   * Track the "productView" event.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   The event to view the product.
+   */
+  public function trackProductView(GetResponseEvent $event) {
+    $product = $this->routeMatch->getParameter('commerce_product');
+    if ($event->getRequest()->getMethod() === 'GET' && !empty($product) && $this->routeMatch->getRouteName() === 'entity.commerce_product.canonical') {
+      $default_variation = $product->getDefaultVariation();
+
+      if ($default_variation) {
+        $this->eventTracker->productDetailViews([$default_variation]);
+      }
+    }
   }
 
   /**
